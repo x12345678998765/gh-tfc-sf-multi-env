f72f97b1c85dabb59a2e85c82a8e14fb15c39416
Dan <tettetran@gmail.com>
2025-01-04 14:42:32 +0900

    Directory test

---
+++ stg/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "tetran"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
+
+resource "snowflake_database" "demo_db2" {
+  name    = "DEMO_DB2"
+  comment = "Database for Snowflake Terraform demo2"
+}
